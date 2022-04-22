d968bfe131048d1ef25eac98bc4272572d24f581
Alessandro Balestra <alexb7396@gmail.com>
2022-04-22 13:44:17 +0200

    aggiunta della libreria jpcap

---
+++ ProvaPacketSniffer/app/src/main/java/com/example/provapacketsniffer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.provapacketsniffer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import jpcap.JpcapCaptor;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
